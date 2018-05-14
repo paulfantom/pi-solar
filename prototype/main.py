f6eb97956d3021900f1b9e253881a647ce533490
paulfantom <pawel@krupa.net.pl>
2018-05-14 19:59:11 +0200

    linter

--- prototype/main.py
+++ prototype/main.py
@@ -4,6 +4,7 @@ import requests
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import time
+import math
 
 DEBUG = True
 EVOK_API = "http://192.168.2.28:8080"
@@ -30,35 +31,36 @@ ADDR_INPUT_MANUAL_MODE = "2"
 
 USE_THERMISTOR = True
 
+
 class Controller():
     temp_sensor_addr_map = {
-        "heater_in"  : ADDR_TEMP_HEATER_IN,
-        "heater_out" : ADDR_TEMP_HEATER_OUT,
-        "solar_in"   : ADDR_TEMP_SOLAR_IN,
-        "solar_out"  : ADDR_TEMP_SOLAR_OUT,
-        "tank_up"    : ADDR_TEMP_TANK_UP,
-        "outside"    : ADDR_TEMP_OUTSIDE,
-        "inside"     : ADDR_TEMP_INSIDE
+        "heater_in":  ADDR_TEMP_HEATER_IN,
+        "heater_out": ADDR_TEMP_HEATER_OUT,
+        "solar_in":   ADDR_TEMP_SOLAR_IN,
+        "solar_out":  ADDR_TEMP_SOLAR_OUT,
+        "tank_up":    ADDR_TEMP_TANK_UP,
+        "outside":    ADDR_TEMP_OUTSIDE,
+        "inside":     ADDR_TEMP_INSIDE
     }
     relays_addr_map = {
-        "co_cwu" : ADDR_RELAY_CO_CWU,  # CWU == TRUE
-        "solar" : ADDR_RELAY_SOL,
-        "pump" : ADDR_RELAY_PUMP,
-        "heater" : ADDR_RELAY_HEAT,
-        "circulation" : ADDR_RELAY_CIRC
+        "co_cwu":      ADDR_RELAY_CO_CWU,  # CWU == TRUE
+        "solar":       ADDR_RELAY_SOL,
+        "pump":        ADDR_RELAY_PUMP,
+        "heater":      ADDR_RELAY_HEAT,
+        "circulation": ADDR_RELAY_CIRC
     }
     mqtt_topics_map = {
-        "actuators"  : MQTT_PREFIX + "/actuators",
+        "actuators":   MQTT_PREFIX + "/actuators",
         "circulation": MQTT_PREFIX + "/circulate/pump",
-        "heater_in"  : MQTT_PREFIX + "/heater/temp_in",
-        "heater_out" : MQTT_PREFIX + "/heater/temp_out",
-        "solar_in"   : MQTT_PREFIX + "/solar/temp_in",
-        "solar_out"  : MQTT_PREFIX + "/solar/temp_out",
-        "solar_up"   : MQTT_PREFIX + "/solar/temp",
-        "solar_pump" : MQTT_PREFIX + "/solar/pump",
-        "tank_up"    : MQTT_PREFIX + "/tank/temp_up",
-        "outside"    : MQTT_PREFIX + "/outside/temp",
-        "inside"     : MQTT_PREFIX + "/room/1/temp_current"
+        "heater_in":   MQTT_PREFIX + "/heater/temp_in",
+        "heater_out":  MQTT_PREFIX + "/heater/temp_out",
+        "solar_in":    MQTT_PREFIX + "/solar/temp_in",
+        "solar_out":   MQTT_PREFIX + "/solar/temp_out",
+        "solar_up":    MQTT_PREFIX + "/solar/temp",
+        "solar_pump":  MQTT_PREFIX + "/solar/pump",
+        "tank_up":     MQTT_PREFIX + "/tank/temp_up",
+        "outside":     MQTT_PREFIX + "/outside/temp",
+        "inside":      MQTT_PREFIX + "/room/1/temp_current"
     }
     thermistor = USE_THERMISTOR
 
@@ -67,7 +69,7 @@ class Controller():
         self.relays = {}
         self.circulation_sensor = False
         self.circulation_last_run = 0
-        self.actuators_value = 0 # Backwards compatibility
+        self.actuators_value = 0  # Backwards compatibility
         self.dual_source = False
         self.external_room_temperature_source = external_room_temp
         self.settings = {
@@ -76,21 +78,21 @@ class Controller():
                 "time_on": 12
             },
             "tank": {
-                "solar_max" : 65.0,
-                "heater_max" : 48.0,
-                "heater_min" : 44.0
+                "solar_max": 65.0,
+                "heater_max": 48.0,
+                "heater_min": 44.0
             },
             "heater": {
                 "hysteresis": 0.5,
-                "critical" : 80.0,
-                "expected" : 20.9,
-                "schedule" : None,
+                "critical": 80.0,
+                "expected": 20.9,
+                "schedule": None,
                 "on": (6, 30),
                 "off": (22, 0)
             },
             "solar": {
                 "critical": 90.00,
-                "on" : 8.0,
+                "on": 8.0,
                 "off": 5.0,
                 "flow": {
                     "s_min": 30,
@@ -122,7 +124,7 @@ class Controller():
             r = requests.get(EVOK_API + "/json/sensor/" + addr)
             value = float(r.json()['data']['value'])
             if DEBUG:
-                new = input("Provide new value for sensor '{}' or press enter to ack value of '{}'".format(sensor, value))
+                new = input("Provide value for sensor '{}' or press enter to ack value of '{}'".format(sensor, value))
                 if new != "":
                     value = float(new)
         try:
@@ -134,29 +136,29 @@ class Controller():
             self.mqtt_publish(sensor, value)
 
     def normalize_thermistor(self, voltage):
-        B = 3950 # Thermistor coefficient (in Kelvin)
+        B = 3950  # Thermistor coefficient (in Kelvin)
         T0 = 273.15 + 25  # Room temperature (in Kelvin)
-        R0 = 100000 # Thermistor resistance at T0
-        VCC = 12 # Voltage applied to resistance bridge
+        R0 = 100000  # Thermistor resistance at T0
+        VCC = 12  # Voltage applied to resistance bridge
         # All calculations are in Kelvin, returned value is in Celsius
         try:
             resistance = (voltage * R0) / (VCC - voltage)
-            inv_T = (1/T0) + (1/B) * log(resistance/R0)
-            return 1 / inv_t - 273.15
-        except:
+            inv_T = (1/T0) + (1/B) * math.log(resistance/R0)
+            return 1 / inv_T - 273.15
+        except (ZeroDivisionError, TypeError):
             return -99
 
     def get_solar_temperature(self):
-        r = requests.get(EVOK_API + "/json/analoginput/" + str(channel))
+        r = requests.get(EVOK_API + "/json/analoginput/1")
         voltage = float(r.json()['data']['value'])
         if self.thermistor:
             return self.normalize_thermistor(voltage)
         else:
-            T_min = 0   # 0V
-            T_max = 200 # 10V
+            T_min = 0    # 0V
+            T_max = 200  # 10V
             try:
                 return (T_max - T_min) * voltage / 10 + T_min
-            except:
+            except (ZeroDivisionError, TypeError):
                 return -99
 
     def update_circulation(self):
@@ -168,41 +170,43 @@ class Controller():
         self.manual = bool(r.json()['data']['value'])
 
     def set_relay(self, relay, state):
-        #if self.relays[relay] == state:
-        #    return
+        if self.relays[relay] == state:
+            return
         addr = self.relays_addr_map[relay]
         data = {
                  "value": str(int(state))
                }
-        for i in range(0,5):
-            r = requests.post(EVOK_API + "/json/relay/" + addr, json = data)
-            if r.status_code == 200: break
+        for i in range(0, 5):
+            r = requests.post(EVOK_API + "/json/relay/" + addr, json=data)
+            if r.status_code == 200:
+                break
         self.relays[relay] = state
         self.actuators()  # BACKWARDS COMPATIBILITY
 
     def set_flow(self, value):
         if value < 0:
-           value = 0
+            value = 0
         if value > 100:
-           value = 100
+            value = 100
         data = {
                  "value": float(value) / 10
                }
-        for i in range(0,5):
-            r = requests.post(EVOK_API + "/json/ao/1", json = data)
-            if r.status_code == 200: break
-        mqtt_publish("solar_pump", value)
+        for i in range(0, 5):
+            r = requests.post(EVOK_API + "/json/ao/1", json=data)
+            if r.status_code == 200:
+                break
+        self.mqtt_publish("solar_pump", value)
 
-    ### HELPER METHODS (unpack old mqtt schema)
+    # HELPER METHODS (unpack old mqtt schema) #
     def actuators(self):
         # zawor reg (regulation)|r  # NOT USED ANYMORE
-        # zawor 0/1     (on/off)|o 
+        # zawor 0/1     (on/off)|o
         # pompa           (pump)|p
         # co/cwu         (water)|w
         # piec          (heater)|h
         # cyrk     (circulation)|c
         # ---rchwpo (8 bit)
-        order = [ "circulation", "heater", "co_cwu", "pump", "solar" ]
+        order = ["circulation", "heater", "co_cwu", "pump", "solar"]
         value = ""
         for relay in order:
             value += str(int(self.relays[relay]))
@@ -211,7 +215,7 @@ class Controller():
             self.actuators_value = v
             self.mqtt_publish("actuators", v)
 
-    ### Settings parser
+    # Settings parser #
     def set_setting(self, mqtt_topic, payload):
         splitted = mqtt_topic.split('/')
         if len(splitted) not in (4, 5):
@@ -232,8 +236,10 @@ class Controller():
             if minor_section in ["on", "off"]:
                 h = int(payload[-4:-2])
                 m = int(payload[-2:])
-                if not 0 <= h < 24: h = 0
-                if not 0 <= m < 60: m = 0
+                if not 0 <= h < 24:
+                    h = 0
+                if not 0 <= m < 60:
+                    m = 0
                 value = (h, m)
             elif minor_section == "schedule":
                 value = str(payload)
@@ -253,16 +259,18 @@ class Controller():
             print("Setting '{}/{}/{}' is changed to {}".format(major_section, minor_section, flow_setting, value))
             self.settings[major_section][minor_section][flow_setting] = value
 
-    ### MQTT SECTION
+    # MQTT SECTION #
     def mqtt_publish(self, instance, value):
-        print("Changed '{}' to value of '{}' on mqtt topic '{}'".format(instance, value, self.mqtt_topics_map[instance]))
+        print("Changed '{}' to value of '{}' on mqtt topic '{}'".format(instance,
+                                                                        value,
+                                                                        self.mqtt_topics_map[instance]))
         publish.single(self.mqtt_topics_map[instance], value, hostname=MQTT_BROKER)
 
-    def mqtt_on_connect(self,client, userdata, flags, rc):
+    def mqtt_on_connect(self, client, userdata, flags, rc):
         client.subscribe("solarControl/+/settings/#")
         client.subscribe("room/1/temp_real")
 
-    def mqtt_on_message(self,client, userdata, msg):
+    def mqtt_on_message(self, client, userdata, msg):
         topic = str(msg.topic)
         if topic == "room/1/temp_real":
             if self.external_room_temperature_source:
@@ -271,7 +279,7 @@ class Controller():
         else:
             self.set_setting(topic, msg.payload)
 
-    ### PROGRAM ###
+    # PROGRAM #
     def calculate_flow(self):
         duty_min = self.settings['solar']['flow']['s_min']
         duty_max = self.settings['solar']['flow']['s_max']
@@ -280,7 +288,8 @@ class Controller():
         T2 = self.temperatures['solar_in']
         T3 = self.temperatures['solar_out']
         delta = T2 - T3
-        if delta >= temp_max: return duty_max
+        if delta >= temp_max:
+            return duty_max
         # y = ax + b
         a = (duty_max - duty_min) / (temp_max - temp_min)
         b = duty_min - temp_min * a
@@ -315,9 +324,9 @@ class Controller():
         T3 = self.temperatures['solar_out']
         T8 = self.temperatures['tank_up']
         if T1 < self.settings['solar']['critical'] and \
-          T3 < T1 and \
-          T8 <= self.settings['tank']['solar_max'] and \
-          T2 - T3 >= self.settings['solar']['off']:
+           T3 < T1 and \
+           T8 <= self.settings['tank']['solar_max'] and \
+           T2 - T3 >= self.settings['solar']['off']:
             if T1 - T3 > self.settings['solar']['on']:
                 self.set_relay("solar", True)
                 self.set_relay("pump", True)
@@ -374,11 +383,10 @@ class Controller():
 
 
 if __name__ == '__main__':
-     c = Controller(True)
-     mqttc = mqtt.Client()
-     mqttc.on_connect = c.mqtt_on_connect
-     mqttc.on_message = c.mqtt_on_message
-     mqttc.connect(MQTT_BROKER)
-     #mqttc.loop_forever()
-     mqttc.loop_start()
-     c.run(0.5)
+    c = Controller(True)
+    mqttc = mqtt.Client()
+    mqttc.on_connect = c.mqtt_on_connect
+    mqttc.on_message = c.mqtt_on_message
+    mqttc.connect(MQTT_BROKER)
+    mqttc.loop_start()
+    c.run(0.5)
